16b0de0a6ec81071f4ab31ad9a235bc425331f91
Alexandre Martins <alexandre.martins.001@gmail.com>
2024-04-01 16:36:33 -0300

    Inicialização do Ambiente

---
+++ src/test/java/br/eti/alexandremartins/springwebstudy/SpringWebStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.alexandremartins.springwebstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
